6cf8d26eb0af75973db00a5aeba4ed862b928ebe
jenya <hypnagonia>
2024-11-27 01:42:13 +0400

    ++

--- src/service/roundScheduler.js
+++ src/service/roundScheduler.js
@@ -16,10 +16,6 @@ const loop = async () => {
     logger.info('Distributing reward')
     const rounds = await loadRounds();
 
-    /**
-        distrubition is not atomic, so during sending the tokens number of people in the channel may change
-        userReward is always calculated according config params amount / number of top users
-     */
     const processRound = async (round) => {
         const usersLeftToReward = round.topUserCount - round.rewardedUsersCount
         if (usersLeftToReward <= 0) {
@@ -51,8 +47,7 @@ const loop = async () => {
             const roundAddress = round.roundAddress
             const assetAddress = round.assetAddress
 
-            // todo decimals
-            const decimals = 18
+            const decimals = +round.decimals || 18
             const amount = (round.amount / usersDataWithValidAddresses.length).toFixed(decimals)
             const fid = usersDataWithValidAddresses[i].fid
             logger.info('Sending reward', { fid, roundAddress, recipientAddress, assetAddress, amount })
@@ -71,6 +66,7 @@ const loop = async () => {
     for (let i = 0; i < rounds.length; i++) {
         const r = rounds[i]
 
+        // todo remove
         if (!+r.lastUpdated && r.roundInterval === '10m') {
             try {
                 await processRound(r)
